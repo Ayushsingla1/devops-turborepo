5b17670c0f5e0b08866153e7ea8ae3c0e0a784da
Ayush singla <143862828+Ayushsingla1@users.noreply.github.com>
2025-06-16 14:06:55 +0530

    turbo repo initialized

---
+++ apps/server/src/index.ts
@@ -0,0 +1,30 @@
+import express, { Request, Response } from "express";
+import cors from "cors"
+import prisma from "@repo/db/client"
+
+const app = express();
+app.use(express.json())
+const PORT = 3000;
+
+app.get('/',(req : Request, res : Response) => {
+    res.send("<h1>Hi there</h1>")
+})
+
+app.post('/user',async(req : Request,res : Response) => {
+
+    const {name , email} = req.body;
+    const result = await prisma.user.create({
+        data : {
+            name,
+            email
+        }
+    })
+
+    console.log(result)
+    res.status(200).json({msg : "success"})
+    return;
+})
+
+app.listen(PORT,() => {
+    console.log("Listening on PORT ",PORT)
+})
---
+++ apps/websocket-server/src/index.ts
@@ -0,0 +1,15 @@
+import { WebSocketServer } from "ws"
+import prisma from "@repo/db/client"
+
+const wss = new WebSocketServer({port: 3001})
+
+wss.on("connection",async(ws) => {
+
+    ws.send("hi there")
+    await prisma.user.create({
+        data : {
+            name : "hi there",
+            email : "siiii"
+        }
+    })
+})
